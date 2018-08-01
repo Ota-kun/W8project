ff1baaee93f8e70ee794c48ea147f304f3fdd02c
Ota-kun <ota-kun@hotmail.com>
2018-08-01 16:40:10 +0300

    kuvaa

--- src/carousel/carousel.component.ts
+++ src/carousel/carousel.component.ts
@@ -14,12 +14,15 @@ export class CarouselComponent implements OnInit {
     this._http.get('https://picsum.photos/list')
     .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
     .subscribe(images => this.images = images);
-}
+  }
 
-private _randomImageUrls(images: Array<{id: number}>): Array<string> {
-return [1, 2, 3].map(() => {
-  const randomId = images[Math.floor(Math.random() * images.length)].id;
-  return `https://picsum.photos/900/500?image=${randomId}`;
-});
+  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+    return [1, 2, 3].map(() => {
+  
+    const randomId = images[Math.floor(Math.random() * images.length)].id;
+  
+    return `https://picsum.photos/900/500?image=${randomId}`;
+    
+    });
   }
 }
